e50853955005365b44e1ca5b8480e4de78b86c6c
Qasim Parekh <153231071+qasimparekh@users.noreply.github.com>
2024-07-19 03:24:46 +0500

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["Express", "ReactJS", "Typescript"];
+  const rightLists = ["React Native", "Next JS", "GraphQL"];
 
   const [copied, setCopied] = useState(false);
 
