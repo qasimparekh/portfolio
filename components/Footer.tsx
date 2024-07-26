216181772de00bb30a9c040386aff9fdfc5adb53
Qasim Parekh <153231071+qasimparekh@users.noreply.github.com>
2024-07-26 22:19:12 +0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link
+
 import { FaLocationArrow } from "react-icons/fa6";
 
 import { socialMedia } from "@/data";
@@ -39,12 +41,13 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div
+            <Link
               key={info.id}
+              href={info.url}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
+            </Link>
           ))}
         </div>
       </div>
